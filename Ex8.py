65fdf8a30d6259eefe0425cab675b66451d71aa9
Chiperi <iulianchiperi2000@gmail.com>
2021-10-29 11:06:46 +0300

    init

---
+++ Ex8.py
@@ -0,0 +1,5 @@
+#Ghicește numărul!
+#Scrieți un program care va genera un număr aleatoriu (google it ;) ) și va cere utilizatorului să ghicească numărul! Programul se va termina când numărul va fi ghicit.
+#   - contorizați numărul de încercări 
+#   - adăugați mesaje ajutătoare ("prea mic" sau "prea mare")
+#   - adăugați mesaj care să indice intervalul curent în care se află numărul, conform încercărilor utilizatorului

b470cfa45cf6927e4312a5309d224ca5a8de114a
Chiperi <iulianchiperi2000@gmail.com>
2021-10-29 11:37:15 +0300

    rezolvat

--- Ex8.py
+++ Ex8.py
@@ -3,3 +3,24 @@
 #   - contorizați numărul de încercări 
 #   - adăugați mesaje ajutătoare ("prea mic" sau "prea mare")
 #   - adăugați mesaj care să indice intervalul curent în care se află numărul, conform încercărilor utilizatorului
+
+import random
+
+x = random.randint(0,100)
+
+y = int(input("Introduceti un numar intre 0 si 100: "))
+incercari = 1
+
+while y != x:
+    print("Nu ati ghicit numarul!")
+    if y < x:
+        print("Numarul introdus este prea mic.")
+        y = int(input("introduceti alt numar: "))
+        incercari += 1
+    if y > x:
+        print("Numarul introdus este prea mare")
+        y = int(input("Introduceti alt numar: "))
+        incercari += 1
+    else:
+        print("Ati ghicit corect!\nNumarul este",y,end = '!\n')
+        print("Ati ghicit in",incercari,"incercari")

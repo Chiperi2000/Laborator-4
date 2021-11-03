ed77c26ad5f9ed08302196fecf2889756744d180
Chiperi <iulianchiperi2000@gmail.com>
2021-11-03 16:23:26 +0200

    rezolvat

--- Ex12.py
+++ Ex12.py
@@ -1,13 +1,8 @@
+#Scrieți un program care va modifica toate vocalele în majuscule.
 
-
-def toggle(phrase):
-    new_phrase = ""
-    for letter in phrase:
-        if letter in ['a', 'e', 'i', 'o', 'u'] or letter in ['A', 'E', 'I', 'O', 'U']:
-            new_phrase += letter.lower()
-        else:
-            new_phrase += letter.upper()
-    return new_phrase
-
-
-print(toggle(input("Enter a phrase: ")))
+text = input("Scrieti textul: ")
+for i in text:
+    if i in 'aeiou':
+        print(i.upper(), end='',)
+    else:
+        print(i,end='')
